14d2f21d19c4474c3881c2692d18273c8de69415
joshua.nieto <joshuanieto2012@gmail.com>
2026-09-18 19:06:12 -0500

    feat: se agg GET /students

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastApi
 from config import APPP_VERSION 
 
-app=FastApi(title="stundets-api", version=APPP_VERSION)
+app=FastApi(title="students-api", version=APPP_VERSION)
 
 @app.get("/healt")
 def healt():
-    return{"status":"ok"}
-    
+    return {"status":"ok"}
+
+@app.get("/students")
+def list_students():
+    return [{"id":1,"name":"John Doe"},{"id":2,"name":"Jane Smith"}]
